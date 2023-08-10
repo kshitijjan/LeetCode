b9b27c5207b2843d305cd89229eed97935a4f60b
Kshitij Jain <64248758+kshitijjan@users.noreply.github.com>
2023-08-10 11:26:04 +0530

    Time: 0 ms (100%), Space: 6.4 MB (48.4%) - LeetHub

---
+++ 1944-truncate-sentence/1944-truncate-sentence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+        int count = 0;
+        string ans = "";
+
+        for(auto i:s){
+            if(i == ' '){
+                count++;
+            }
+            if(count == k){
+                break;
+            }
+            ans += i;
+        }
+        return ans;
+
+    }
+};
