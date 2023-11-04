d76f8e666d1a773881fbca377baa1b73357c7ee6
Kshitij Jain <64248758+kshitijjan@users.noreply.github.com>
2023-11-04 11:21:27 +0530

    Time: 3 ms (97.99%), Space: 22.3 MB (6.21%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -10,8 +10,9 @@
  * };
  */
 class Solution {
-    private:
-    void inorder(TreeNode* root, vector<int> &v){
+    public:
+    void inorder(TreeNode* root, vector<int>& v ){
+
         if(root == NULL){
             return;
         }
@@ -22,14 +23,15 @@ class Solution {
 public:
     bool isValidBST(TreeNode* root) {
         
-        vector<int> v;
         if(root == NULL){
             return true;
         }
+        vector<int> v;
+
         inorder(root, v);
 
         for(int i=1; i<v.size(); i++){
-            if(v[i-1]>=v[i]){
+            if(v[i]<=v[i-1]){
                 return false;
             }
         }
